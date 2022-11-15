0571ca70b794129242554c62e22c471641339a7e
Gavrosh Cyril <gavrosh.cyril@gmail.com>
2022-11-15 22:55:18 +0300

    feat: create a Map page

--- src/pages/Map/Map.jsx
+++ src/pages/Map/Map.jsx
@@ -1,13 +1,21 @@
 import React from "react";
 import "./Map.css";
-import MapImg from "../../Assets/Images/shema-metro.jpg";
+import MapImg from "../../Assets/Images/mapMetro.jpg";
+import MapLabel from "../../Assets/Images/mapLabel.png";
 
 const Map = () => {
   return (
-    <div className="Map">
-      <div className="Map-container">
-        <h2 className="Map-title">Схема метрополитена</h2>
-        <img className="Map-img" src={MapImg} alt="Shema Metro" />
+    <div className="MapWrapper">
+      <div className="Map">
+        <div className="Map-container">
+          <h2 className="Map-title">
+            Схема линий метро Минского метрополитена
+          </h2>
+        </div>
+      </div>
+      <div className="Map-info">
+        <img src={MapLabel} alt="" className="Map-label" />
+        <img src={MapImg} alt="" className="Map-img" />
       </div>
     </div>
   );
